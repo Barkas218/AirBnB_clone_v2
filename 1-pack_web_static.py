0e56813195d8de531250e7ec48b39a6e1d4c5570
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-03 22:08:28 +0000

    Oh god

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from fabric.api import local
-from datatime import datatime
+from datetime import datetime
 
 def do_pack():
-    	""" Creats a trgz archive """
-	file_name = "{}_{}{}{}{}{}{}.tgz".format("web_static", time.year,
-						time.month, time.day,
-						time.hour, time.minute,
-						time.second)
-	local("mkdir -p versions")
-	local("tar -czvf versions/{} {}".format(file_name, "web_static")
+    """ Creats a trgz archive """
+    time = datetime.now()
+    file_name = "{}_{}{}{}{}{}{}.tgz".format("web_static", time.year,
+                                            time.month, time.day,
+                                            time.hour, time.minute,
+                                            time.second)
+    local("mkdir -p versions")
+    local("tar -cavf versions/{} {}".format(file_name, "web_static")
