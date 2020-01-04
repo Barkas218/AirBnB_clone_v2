d0c51d0ba8116895f45ef567910f4845e697a0d6
DNeosphere <daniela.gomezp05@outlook.com>
2020-01-04 01:13:32 +0000

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,16 +4,15 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ["35.196.2.249", "35.196.95.206"]
-
+env.hosts = ["35.231.251.156", "35.243.171.176"]
 
 def do_pack():
     """ Creats a trgz archive """
     time = datetime.now()
     file_name = "{}_{}{}{}{}{}{}.tgz".format("web_static", time.year,
-                                             time.month, time.day,
-                                             time.hour, time.minute,
-                                             time.second)
+                                            time.month, time.day,
+                                            time.hour, time.minute,
+                                            time.second)
     local("mkdir -p versions")
     local("tar -cavf versions/{} {}".format(file_name, "web_static"))
 
@@ -32,23 +31,22 @@ def do_deploy(archive_path):
 
         """Uncompress the archive to the folder """
         run("mkdir -p /data/web_static/releases/{}".format(short_name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-            .format(full_name, short_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(full_name, short_name))
         run("rm /tmp/{}".format(full_name))
         run("mv /data/web_static/releases/{}/web_static/* \
         /data/web_static/releases/{}/".format(short_name, short_name))
 
         """ Delete the archive from the web server """
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(short_name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(short_name))
 
+        """ Delete the symbolic link /data/web_static/current from the web server """
         run("rm -rf /data/web_static/current")
 
         """ create symlink """
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(short_name))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(short_name))
 
         print("New version deployed!")
 
     except Exception:
         return False
+
