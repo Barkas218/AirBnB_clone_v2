52d962618ee9bd8c626903e042d4332f412d6165
Dani Gomez <daniela.gomezp05@outlook.com>
2020-01-22 15:15:19 -0500

    Feature: task 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from models import State
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def show_states_list():
+    """ Gets states list """
+    state_l = storage.all('State')
+    return render_template('7-states_list.html', state_l=state_l)
+
+@app.route('/states/<id>', strict_slashes=False)
+def show_states_id(id):
+    """ Shows states id """
+    state_l = storage.all('State')
+    return render_template('9-states.html', id=id, state_l=state_l)
+
+
+
+@app.teardown_appcontext
+def tear_down_db(n):
+    """ Closes the sql session """
+    storage.close()
