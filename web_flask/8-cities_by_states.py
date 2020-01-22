8d0e0d270a57280806273b648eb5d0e104ff4540
Dani Gomez <daniela.gomezp05@outlook.com>
2020-01-22 14:31:38 -0500

    Feature: task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from models import State
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def show_states_list():
+    """ Gets states list """
+    state_l = storage.all('State')
+    return render_template('8-cities_by_states.html', state_l=state_l)
+
+
+@app.teardown_appcontext
+def tear_down_db(n):
+    """ Closes the sql session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run()
